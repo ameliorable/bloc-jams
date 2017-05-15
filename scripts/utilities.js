8515af108de93bfc7031bf0e66be1ebff73072ce
Carolyn Walcott <carolynoliveira@Carolyns-MacBook-Air.local>
2017-05-14 21:12:53 -0400

    Checkpoint 10 - DOM Scripting: Collection View - Completed

---
+++ scripts/utilities.js
@@ -0,0 +1,6 @@
+function forEach(array, callback) {
+    for(var i = 0; i < array.length; i++) {
+       callback(array[i]);
+    }
+}
+//I was kind of close before, but this now matches the solution video//
